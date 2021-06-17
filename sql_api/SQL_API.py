f2c306a31452876d7f495c1ab004fe2e6a4ceabb
Themishau <40619652+Themishau@users.noreply.github.com>
2021-06-17 11:19:50 +0200

    Add files via upload

--- sql_api/SQL_API.py
+++ sql_api/SQL_API.py
@@ -6,9 +6,6 @@ import random
 import pandas as pd
 from enum import Enum
 
-
-
-
 class TABLENAMES(Enum):
     # mit TABLENAMES.QR_CODE.name -> 'QR_CODE'
     # mit TABLENAMES.QR_CODE.value -> 'QR_code'
@@ -70,11 +67,9 @@ class SQL_Writer():
                   """
 
         for i in result_gesch:
-
             benefit_len = len(result_bene)
             benefit_len_list = list(range(benefit_len))
             random.shuffle(benefit_len_list)
-
             r = []
             r.append(benefit_len_list[0])
             r.append(benefit_len_list[1])
@@ -86,7 +81,7 @@ class SQL_Writer():
                 self.cursor.execute(sql, (i[1], result_bene[j][1]))
 
         self.db_connection.commit()
-        self.db_connection.close()
+        self.close_connection()
 
     def add_dummy_data_to_geschaeft(self):
 
@@ -97,7 +92,6 @@ class SQL_Writer():
                      VALUES
                         (%s)
                   """
-
         for i in df.itertuples():
             try:
                 self.cursor.execute(sql, i.RheinBergGalerieGeschaeft)
@@ -112,8 +106,9 @@ class SQL_Writer():
         for i in result:
             print(i)
 
-    def add_dummy_data_to_benefits(self):
+        self.close_connection()
 
+    def add_dummy_data_to_benefits(self):
         df = pd.read_csv('some_data/testdata2.csv', names=['RheinBergGalerieBenefit'], delimiter=';')
         # print(df['RheinBergGalerieGeschaeft'])
         sql = """INSERT INTO `RheinBerg_QRCode`.`Benefit`
@@ -131,6 +126,7 @@ class SQL_Writer():
                 print(e)
                 print('konnte nicht speichern:')
                 print(i.RheinBergGalerieBenefit)
+        self.close_connection()
 
     def find_in_tuple_by_id(self, tup, id):
         for i in tup:
@@ -165,12 +161,10 @@ class SQL_Writer():
             print(str(gb.iloc[0].Geschaeft))
             print(str(gb.iloc[0].Benefit))
             print('---------------')
-        
 
+        self.close_connection()
 
     def add_dummy_data_to_bi_light(self):
-
-
         cursor = self.db_connection.cursor()
         qr_codes = self.get_df_select_qr_code()
         # sql = "SELECT * FROM `geschaeft_benefit`"
@@ -189,7 +183,6 @@ class SQL_Writer():
                          %s)
                   """
 
-
         for i in qr_codes.itertuples():
             try:
                 ran = random.randint(112, 222)
@@ -204,8 +197,8 @@ class SQL_Writer():
                 print(str(gb.iloc[0].Geschaeft))
                 print(str(gb.iloc[0].Benefit))
                 print('---------------')
-    
-    
+        self.close_connection()
+
     def add_dummy_data_to_qr_code(self):
         print("add")
         delimiter = '-'
@@ -245,64 +238,112 @@ class SQL_Writer():
                 print(e)
                 print('konnte nicht speichern:')
                 print(string_new)
-            j += 1    
-    
+            j += 1
+        self.close_connection()
+
     def select_benefits(self):
         sql = "SELECT * FROM `Benefit`"
         self.cursor.execute(sql)
         result = self.cursor.fetchall()
         print('Result: ')
         print(pd.DataFrame(list(result), columns=["Id", "Benefit", "Timestamp"]))
+        self.close_connection()
 
     def select_geschaefte(self):
         sql = "SELECT * FROM `Geschaeft`"
         self.cursor.execute(sql)
         print('Result: ')
         print(pd.DataFrame(list(self.cursor.fetchall()), columns=["Id", "Geschaeft", "Timestamp"]))
+        self.close_connection()
 
     def select_gesch_bene(self):
         sql = "SELECT * FROM `geschaeft_benefit`"
         self.cursor.execute(sql)
         print('Result: ')
         print(pd.DataFrame(list(self.cursor.fetchall()), columns=["Id", "Geschaeft", "Benefit", "Timestamp"]))
-        
+        self.close_connection()
+
     def select_bi_light(self):
         sql = "SELECT * FROM `BI_Light`"
         self.cursor.execute(sql)
         print('Result: ')
         print(pd.DataFrame(list(self.cursor.fetchall()), columns=["Id", "Geschaeft", "Benefit", "Timestamp"]))
+        self.close_connection()
 
     def select_qr_code(self):
         sql = "SELECT * FROM `QR_Code`"
         self.cursor.execute(sql)
         print('Result: ')
         print(pd.DataFrame(list(self.cursor.fetchall()), columns=["Id", "QR_Code", "Timestamp"]))
+        self.close_connection()
 
-    # returns a dataframe, full
+    def select_qr_code_count(self):
+        sql = "SELECT * FROM `qr_code_count`"
+        self.cursor.execute(sql)
+        print('Result: ')
+        print(pd.DataFrame(list(self.cursor.fetchall()), columns=["Id", "qr_code_count", "Timestamp"]))
+        self.close_connection()
+
+    # # # # returns a dataframe, full # # # #
     def get_df_select_benefits(self):
         sql = "SELECT * FROM `Benefit`"
         self.cursor.execute(sql)
+        self.close_connection()
         return pd.DataFrame(list(self.cursor.fetchall()), columns=["Id", "Benefit", "Timestamp"])
 
     def get_df_select_geschaefte(self):
         sql = "SELECT * FROM `Geschaeft`"
         self.cursor.execute(sql)
+        self.close_connection()
         return pd.DataFrame(list(self.cursor.fetchall()), columns=["Id", "Geschaeft", "Timestamp"])
 
     def get_df_select_gesch_bene(self):
         sql = "SELECT * FROM `geschaeft_benefit`"
         self.cursor.execute(sql)
+        self.close_connection()
         return pd.DataFrame(list(self.cursor.fetchall()), columns=["Id", "Geschaeft", "Benefit", "Timestamp"])
 
     def get_df_select_bi_light(self):
         sql = "SELECT * FROM `BI_Light`"
         self.cursor.execute(sql)
+        self.close_connection()
         return pd.DataFrame(list(self.cursor.fetchall()), columns=["Id", "Geschaeft", "Benefit", "Timestamp"])
 
     def get_df_select_qr_code(self):
         sql = "SELECT * FROM `QR_Code`"
         self.cursor.execute(sql)
+        self.close_connection()
         return pd.DataFrame(list(self.cursor.fetchall()), columns=["Id", "QR_Code", "Timestamp"])
+
+    def get_df_select_qr_code_count(self):
+        sql = "SELECT * FROM `qr_code_count`"
+        self.cursor.execute(sql)
+        self.close_connection()
+        return pd.DataFrame(list(self.cursor.fetchall()), columns=["Id", "qr_code_count", "Timestamp"])
+
+    # wenn duplicate, dann wird false returnt
+    def write_qr_code_count_in_database(self, qr_code_count):
+        # format: 'https://ergebnis.lola-coronapass.de/372e7c02-9c3a-4082-971d-6e0578ddea81'
+
+        try:
+            sql = """INSERT INTO `RheinBerg_QRCode`.`qr_code_count`
+                            (`qr_code_count`)
+                         VALUES
+                            (%s)
+                      """
+            cursor = self.db_connection.cursor()
+            cursor.execute(sql, qr_code_count)
+
+            self.db_connection.commit()
+            self.close_connection()
+            return True
+
+        except pymysql.err.IntegrityError as e:
+            print(e)
+            print('konnte nicht speichern:')
+            print(qr_code_count)
+            self.close_connection()
+            return False
 
     # wenn duplicate, dann wird false returnt
     def write_qr_code_in_database(self, qr_code):
@@ -318,17 +359,18 @@ class SQL_Writer():
                           """
                 cursor = self.db_connection.cursor()
                 cursor.execute(sql, qr_code)
-                
-
+                self.add_dummy_data_to_bi_light_singe_row(qr_code)
                 self.db_connection.commit()
-                
+
 
             except pymysql.err.IntegrityError as e:
                 print(e)
                 print('konnte nicht speichern:')
                 print(qr_code)
         else:
+            self.close_connection()
             return False
+        self.close_connection()
         return True
 
     def check_if_not_duplicate_qrcode(self, where_clausel):
@@ -343,18 +385,20 @@ class SQL_Writer():
         if not df.empty:
             print('Result: ')
             print(df)
-        
+        self.close_connection()
         return isempty
 
 
     def create_connection_pymysql(self):
-        connection = pymysql.connect(host='192.168.178.25',    # change host-ip if needed
+        connection = pymysql.connect(host='localhost',    # change host-ip if needed
                                      port=3310,           # change port if needed
                                      user='dummy_insert',
                                      password='1234',
                                      db='RheinBerg_QRCode')
         print('success')
+        self.close_connection()
         return connection
 
     def close_connection(self):
-        self.db_connection.close()
+        # self.db_connection.close()
+        print()
